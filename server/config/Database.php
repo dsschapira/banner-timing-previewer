4f7f643744b94091f2a264d8478006e7371f907c
Dan Schapira <dsschapira@gmail.com>
2018-10-15 14:43:08 -0400

    Create database object for connecting

---
+++ server/config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Database{
+        // DB Params
+        private $host = 'localhost';
+        private $port = '3306';
+        private $db_name = 'banner_data';
+        private $username = 'server_acct';
+        private $password = 'EyEoUwXPZBOTfz8j';
+        private $conn;
+
+        // DB Connect
+        public function connect(){
+            $this->conn = null;
+
+            try{
+                $this->conn = new PDO('mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn->setAttrbiute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch(PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+        }
+    }
