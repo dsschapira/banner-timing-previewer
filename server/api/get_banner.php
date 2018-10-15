6d91804799ded1d9af0df3a6dae2d205cfeb9df3
Dan Schapira <dsschapira@gmail.com>
2018-10-15 14:59:28 -0400

    Fixed endpoint bugs

--- server/api/get_banner.php
+++ server/api/get_banner.php
@@ -3,8 +3,8 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
 
-    include_once '../../config/Database.php';
-    include_once '../../models/Banner.php';
+    include_once '../config/Database.php';
+    include_once '../models/Banner.php';
 
     // Instantiate DB & connect
     $database = new Database();
--- server/config/Database.php
+++ server/config/Database.php
@@ -13,9 +13,9 @@
         public function connect(){
             $this->conn = null;
 
-            try{
+            try{ 
                 $this->conn = new PDO('mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name, $this->username, $this->password);
-                $this->conn->setAttrbiute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
             }
